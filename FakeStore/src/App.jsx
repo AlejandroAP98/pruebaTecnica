0c6161b48be04195951992f7c592825c13f78ac7
AlejandroAP98 <aalvarezpa@unal.edu.co>
2024-02-15 20:54:22 -0500

    se instala biblioteca flowbite para usar componentes, se consume API general

--- FakeStore/src/App.jsx
+++ FakeStore/src/App.jsx
@@ -1,11 +1,39 @@
+import { useState, useEffect } from "react";
+import Item from "./Item";
 
 function App() {
-  
+  const [products, setProducts] = useState([]);
+  // const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setProducts(data);
+        // setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        // setLoading(false);
+      });
+  }, []);
+
+  // if (loading) {
+  //   return <h1>Cargando...</h1>;
+  // }
+
   return (
+    <>
       <div className='bg-red-400'>
         <h1 className='text-6xl text-center'>Hello World</h1>
       </div>
-  )
+      {products.map(product => (
+        <Item key={product.id} titulo={product.title} precio={product.price} imagenes={product.images} />
+      ))}
+      
+    </>
+  );
 }
 
-export default App
+export default App;
